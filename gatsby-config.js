64c830ed49cfb9864216267e9a8952284e0fc0bf
Tim Moloney <timpbook@Tim-MacBook-Pro.local>
2023-07-06 13:49:09 +1000

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,13 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
 module.exports = {
   siteMetadata: {
-    title: `gatsbyv1`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "My First Gatsby Site",
   },
-  plugins: [],
-}
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
+    <Layout pageTitle="Home Page With Picture">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Cows in the paddock"
+        src="../images/cows.jpg"
+      />
     </Layout>
   )
 }
