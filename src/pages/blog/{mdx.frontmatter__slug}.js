8f9d4acf9e385b97dd9338b7371955b08fa5fd3d
Tim Moloney <timpbook@Tim-MacBook-Pro.local>
2023-07-08 12:30:06 +1000

    Finished Gatsby Tutorial Part 6

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,42 +1,28 @@
 import * as React from 'react'
-import { Link, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 
-const BlogPage = ({ data }) => {
+const BlogPost = ({ data, children }) => {
   return (
-    <Layout pageTitle="My Blog Posts">
-      {
-        data.allMdx.nodes.map(node => (
-          <article key={node.id}>
-            <h2>
-              <Link to={`/blog/${node.frontmatter.slug}`}>
-                {node.frontmatter.title}
-              </Link>
-            </h2>
-            <p>Posted: {node.frontmatter.date}</p>
-          </article>
-        ))
-      }
+    <Layout pageTitle={data.mdx.frontmatter.title}>
+      <p>{data.mdx.frontmatter.date}</p>
+      {children}
     </Layout>
   )
 }
 
 export const query = graphql`
-  query {
-    allMdx(sort: { frontmatter: { date: DESC }}) {
-      nodes {
-        frontmatter {
-          date(formatString: "MMMM D, YYYY")
-          title
-          slug
-        }
-        id
+  query ($id: String) {
+    mdx(id: {eq: $id}) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
       }
     }
   }
 `
 
-export const Head = () => <Seo title="My Blog Posts" />
+export const Head = ({ data }) => <Seo title={data.mdx.frontmatter.title} />
 
-export default BlogPage
+export default BlogPost
